bd57b973d2e6b40592ff5667f9f799f2e27ceadb
Bogdan Popa <popa.bogdanp@gmail.com>
2013-08-10 21:18:50 +0300

    add Lexer and LexerSpec

---
+++ src/test/scala/schemer/LexerSpec.scala
@@ -0,0 +1,12 @@
+package schemer
+
+import org.specs2.mutable._
+
+class LexerSpec extends Specification {
+  "The Lexer" should {
+    // TODO: Implement actual tests.
+    "pass" in {
+      "pass" must have size(4)
+    }
+  }
+}
