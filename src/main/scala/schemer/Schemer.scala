afd5a158e79a82500d84b4cf53092ffe1cea3d59
Bogdan Popa <popa.bogdanp@gmail.com>
2013-08-11 07:16:41 +0300

    make evaluation smarter

--- src/main/scala/schemer/Schemer.scala
+++ src/main/scala/schemer/Schemer.scala
@@ -1,35 +1,33 @@
 package schemer
 
-// TODO: Clean this up.
 object Schemer {
-  def eval(expression: Expression, env: Env): Either[String, (Expression, Env)] =
+  type Expressions = Seq[Expression]
+  type EvalResult  = Either[String, (Expression, Env)]
+  type EvalResults = Either[String, (Expressions, Env)]
+
+  def eval(source: String, input: String, env: Env): EvalResult =
+    Parser(source, input).right.flatMap {
+      case (_, xs) => {
+        withEvalList(xs, env) {
+          case (xs, env) =>
+            Right(xs.last, env)
+        }
+      }
+    }
+
+  def eval(expression: Expression, env: Env): EvalResult =
     expression match {
       case e: ApplicationExpression => {
         eval(e.f, env) match {
-          //case Right((macro: MacroExpression, env)) => {
-          //}
+          case Right((macro_ : MacroExpression, env)) =>
+            evalBody(macro_.body, paramsToEnv(macro_.ps.xs, e.ps, env))
           case Right((fn: FunctionExpression, env)) =>
-            withEvalList(e.ps, env) { ps =>
-              val fnEnv = fn.ps.xs.zipWithIndex.foldLeft(env) {
-                case (env, (sym, idx)) =>
-                  sym match {
-                    case s: SymbolExpression    => env.set(s, ps(idx))
-                    case s: ExpSymbolExpression => env.set(
-                      SymbolExpression(s.s),
-                      ListExpression(ps.slice(idx, ps.length)))
-                  }
-              }
-
-              val body = withEvalList(fn.body, fnEnv) { xs =>
-                Right((ListExpression(xs), env))
-              }
-
-              body match {
-                case Right((body: ListExpression[_], env)) => Right((body.xs.last, env))
-                case Left(err)                             => Left(err)
-              }
+            withEvalList(e.ps, env) {
+              case (ps, env) =>
+                evalBody(fn.body, paramsToEnv(fn.ps.xs, e.ps, env))
             }
           case Left(err) => Left(err)
+          case _         => undefinedState
         }
       }
       case e: SymbolExpression =>
@@ -38,8 +36,9 @@ object Schemer {
           case None    => Left(s"failed to look up '${e.s}'")
         }
       case e: ListExpression[_] =>
-        withEvalList(e.xs, env) { xs =>
-          Right((ListExpression(xs), env))
+        withEvalList(e.xs, env) {
+          case (xs, env) =>
+            Right((ListExpression(xs), env))
         }
       case e: UnqotedExpression   => eval(e.e, env)
       case e: ExpSymbolExpression => eval(SymbolExpression(e.s), env)
@@ -49,27 +48,50 @@ object Schemer {
       case e                      => Right(e, env)
     }
 
-  protected def evalList(xs: Seq[Expression], env: Env): Either[String, Seq[Expression]] = {
-    var fail: String = ""
-    val rs = xs.map { x =>
-      eval(x, env) match {
-        case Right((v, env)) => Some(v)
-        case Left(err)       => {
-          if (fail isEmpty) fail = err
-          None
+  protected def evalList(xs: Expressions, env: Env): EvalResults = {
+    val empty: EvalResults = Right((Seq[Expression](), env))
+
+    (empty /: xs) {
+      case (Left(err), _)        => Left(err)
+      case (Right((xs, env)), x) => {
+        eval(x, env).right.flatMap {
+          case (v, env) => Right((xs ++ Seq(v), env))
         }
       }
     }
-
-    if (fail isEmpty) Right(rs.flatten)
-    else Left(fail)
   }
 
-  protected def withEvalList(xs: Seq[Expression], env: Env)
-                            (fn: Seq[Expression] => Either[String, (Expression, Env)]):
-      Either[String, (Expression, Env)] =
-    evalList(xs, env) match {
-      case Right(xs) => fn(xs)
-      case Left(err) => Left(err)
+  protected def withEvalList
+      (xs: Expressions, env: Env)
+      (fn: (Seq[Expression], Env) => EvalResult):
+      EvalResult =
+    evalList(xs, env).right.flatMap {
+      case (xs, env) => fn(xs, env)
     }
+
+  protected def paramsToEnv(ss: Seq[Expression], ps: Seq[Expression], env: Env): Env =
+    ss.zipWithIndex.foldLeft(env) {
+      case (env, (sym, idx)) =>
+        sym match {
+          case s: SymbolExpression    => env.set(s, ps(idx))
+          case s: ExpSymbolExpression => env.set(
+            SymbolExpression(s.s),
+            ListExpression(ps.slice(idx, ps.length)))
+          case _ => undefinedState
+        }
+    }
+
+  protected def evalBody(xs: Seq[Expression], env: Env): EvalResult = {
+    val body = withEvalList(xs, env) {
+      case (xs, env) => Right((ListExpression(xs), env))
+    }
+
+    body.right.flatMap {
+      case (body: ListExpression[_], env) =>
+        Right((body.xs.last, env))
+      case _ => undefinedState
+    }
+  }
+
+  protected def undefinedState = sys.error("Should never have gotten here.")
 }
