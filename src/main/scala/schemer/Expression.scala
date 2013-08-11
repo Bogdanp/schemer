cdcd1907789eb9310aa29ad0deb1b78dd73b8d3c
Bogdan Popa <popa.bogdanp@gmail.com>
2013-08-11 08:40:29 +0300

    add toString method to Expression

--- src/main/scala/schemer/Expression.scala
+++ src/main/scala/schemer/Expression.scala
@@ -1,6 +1,31 @@
 package schemer
 
-sealed trait Expression
+sealed trait Expression {
+  override def toString: String =
+    this match {
+      case BooleanExpression(true)  => "#t"
+      case BooleanExpression(false) => "#f"
+      case NumberExpression(n)      => n.toString
+      case StringExpression(s)      => "\"" + s + "\""
+      case SymbolExpression(s)      => s
+      case ExpSymbolExpression(s)   => "&" + s
+      case ListExpression(xs)       => s"[${xs.map(_.toString).mkString(" ")}]"
+      case QuotedExpression(e)      => "'" + e.toString
+      case UnqotedExpression(e)     => "@" + e.toString
+
+      case MacroExpression(s, ps, body) =>
+        s"""|(defmacro ${s.toString} ${ps.toString}
+            |  ${body.map(_.toString).mkString("\n  ")})""".stripMargin
+
+      case FunctionExpression(s, ps, body) =>
+        s"""|(defn ${s.toString} ${ps.toString}
+            |  ${body.map(_.toString).mkString("\n  ")})""".stripMargin
+
+      case ApplicationExpression(f, ps) =>
+        s"""(${f.toString} ${ps.map(_.toString).mkString(" ")})"""
+    }
+}
+
 case class BooleanExpression(b: Boolean) extends Expression
 case class NumberExpression(n: Double) extends Expression
 case class StringExpression(s: String) extends Expression
